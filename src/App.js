8faba653edacb911963124c102813f56e6277dc4
R-M-G <ryan.gregory@ryanmgregory.com>
2020-10-11 22:22:33 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       
+      </header>
+      <h1>App</h1>
+    </div>
+  );
+}
+
+export default App;
